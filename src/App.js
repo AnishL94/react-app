5a15ae51c644705b6ae8ea02030d0293cd86bad6
limbu <Limbu@Anishs-MacBook-Pro.local>
2023-04-05 21:15:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid"
+import ToDoList from "./components/ToDoList";
+
+
+// {
+//   id: 1,
+//   name: 'ToDo1',
+//   complete: false,
+// }
+const LOCAL_STORAGE_KEY = 'todosApp.todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const toDoNameRef = useRef()
+
+  useEffect(() =>{
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function handleAddToDo(e) {
+    const name = toDoNameRef.current.value
+    if(name === '') return 
+
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+
+    toDoNameRef.current.value = null
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToDoList todos={todos} />
+      <input ref={toDoNameRef} type="text" />
+      <button onClick={handleAddToDo}>Add To Do</button>
+      <button>Clear Completed</button>
+      <div>0 tasks to do</div>
+    </>
   );
 }
 
